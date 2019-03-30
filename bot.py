76b5da5e07983166d8706767c64ea2ab14ea7404
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-03-30 11:40:24 +0530

    Added home route

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask import request as req
+from flask import render_template
 import githubbot.utils as gb
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def recv_webhook():
-    print(req.get_json())
-    return
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('Hello')
 
+@app.route('/recv', methods=['POST'])
+def recv_webhook_event():
+    return req.get_json()
 
 def make_gh_request(endpoint):
     r = requests.get(endpoint)
